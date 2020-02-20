95f8392fc5f52791e7b9183c41dd35bb0ce39202
Martha Cryan <martha.cryan@ibm.com>
2020-02-20 10:28:24 -0600

    Removed a line that was specific to my machine

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  testEnvironment: "node",
-  transformIgnorePatterns: ['/Users/martha.cryanibm.com/gitrepos/elyra/node_modules/', '/Users/martha.cryanibm.com/gitrepos/elyra/tests/node_modules']
+  testEnvironment: "node"
 };
